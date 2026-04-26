d4d9303d8224815a467a17e06e184db40e4f8e90
Jonnavithula teja srivaishnavi <100864239+jteja2003@users.noreply.github.com>
2026-04-26 16:33:10 +0530

    Create twonumadd.java

---
+++ twonumadd.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class main{
+  public static void main(String []args){
+    int a,b ;
+    Scanner sc = new Scanner(System.in);
+    a=sc.nextInt() ; b=sc.nextInt();
+    System.out.println(a+b);
+  }
+}
