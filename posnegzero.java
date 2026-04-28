108175d09478d2b65f5f45fe637b96d9e1e3b62b
Jonnavithula teja srivaishnavi <100864239+jteja2003@users.noreply.github.com>
2026-04-28 18:15:54 +0530

    Create posnegzero.java

---
+++ posnegzero.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class main{
+  public static void main(String[] args){
+    int n= new java.util.Scanner(System.in).nextInt();
+    System.out.println(n==0?"zero":(n>0?"positive":"negative"));
+    
+  }
+}
